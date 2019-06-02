c299d75deebc3b4384ea2502786f6eea54127d1f
Encasherr <encasherr@gmail.com>
2019-06-02 21:54:57 +0530

    Fix candidate controller

--- src/Controllers/admin/CandidateController.js
+++ src/Controllers/admin/CandidateController.js
@@ -4,43 +4,43 @@ import { EmailConfig } from '../../commons/ServerConfig';
 import AdminTestController from './AdminTestController';
 
 class CandidateController {
-    // GetAll = (req, resp) => {
-    //     console.log('get all candidates called');
-    //     let candidates = this.initializeCollection();
-    //     console.log(candidates.data.length);
-    //     resp.send(candidates.data);
-    // }
+    GetAll = (req, resp) => {
+        console.log('get all candidates called');
+        let candidates = this.initializeCollection();
+        console.log(candidates.data.length);
+        resp.send(candidates.data);
+    }
 
-    // Add = (req, resp) => {
-    //     console.log('Add Candidate called');
-    //     console.log(req.body);
-    //     let candidates = this.initializeCollection();
-    //     candidates.insert(req.body);
-    //     db.saveDatabase();
-    //     resp.send(JSON.stringify(req.body));
-    // }
+    Add = (req, resp) => {
+        console.log('Add Candidate called');
+        console.log(req.body);
+        let candidates = this.initializeCollection();
+        candidates.insert(req.body);
+        db.saveDatabase();
+        resp.send(JSON.stringify(req.body));
+    }
 
-    // Update = (req, resp) => {
-    //     console.log('update candidate called');
-    //     console.log(req.body);
-    //     let candidates = this.initializeCollection();
-    //     let candidateToUpdate = candidates.find({ '$loki': req.body.$loki });
-    //     if(candidateToUpdate && candidateToUpdate.length > 0) {
-    //         candidateToUpdate[0].title = req.body.title;
-    //         candidateToUpdate[0].description = req.body.description;
-    //         candidates.update(candidateToUpdate[0]);
-    //     }
-    //     else {
-    //         console.log('nothing to update');
-    //     }
-    //     console.log(candidateToUpdate);
-    //     resp.send(JSON.stringify(req.body));
-    // }
+    Update = (req, resp) => {
+        console.log('update candidate called');
+        console.log(req.body);
+        let candidates = this.initializeCollection();
+        let candidateToUpdate = candidates.find({ '$loki': req.body.$loki });
+        if(candidateToUpdate && candidateToUpdate.length > 0) {
+            candidateToUpdate[0].title = req.body.title;
+            candidateToUpdate[0].description = req.body.description;
+            candidates.update(candidateToUpdate[0]);
+        }
+        else {
+            console.log('nothing to update');
+        }
+        console.log(candidateToUpdate);
+        resp.send(JSON.stringify(req.body));
+    }
 
-    // Delete = (req, resp) => {
-    //     console.log('get all candidates called');
-    //     resp.send('get all candidates called');
-    // }
+    Delete = (req, resp) => {
+        console.log('get all candidates called');
+        resp.send('get all candidates called');
+    }
 
     SendInvite = (req, resp) => {
         console.log('send invite called');
