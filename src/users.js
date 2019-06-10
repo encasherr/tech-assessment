cebc8930a81ce3bb9cd4381d4a0e01d30519496f
encasherr <47582140+encasherr@users.noreply.github.com>
2019-06-10 09:27:27 +0530

    Add update method

--- src/users.js
+++ src/users.js
@@ -28,6 +28,38 @@ class Users {
         return null;
     }
 
+    UpdateUser = (emailId, newEntity) => {
+        let users = this.initializeCollection();
+        let filteredUsers = users.where((item) => {
+            //console.log(`item: ${item['$loki']}, testId: ${testId}, result: ${item['$loki'] == testId}`); 
+            return item['emailId'] == emailId;    
+        });
+        console.log(emailId);
+        if(filteredUsers && filteredUsers.length > 0) {
+            let userToUpdate = filteredUsers[0];
+            
+            let entityToUpdate = this.replaceEntity(userToUpdate, newEntity);
+            users.update(entityToUpdate);
+            db.saveDatabase();
+            return entityToUpdate;
+        }
+        else {
+            console.log('nothing to update');
+            return null;
+        }
+    }
+
+    replaceEntity = (oldEntity, newEntity) => {
+        if(oldEntity != null){
+            for (var property in newEntity) {
+                if (newEntity.hasOwnProperty(property) && property !== "$loki" && property !== 'meta') {
+                    oldEntity[property] = newEntity[property];
+                }
+            }
+        }
+        return oldEntity;
+    }
+
     initializeCollection = () => {
         let users = db.getCollection('users');
         if(!users) {
